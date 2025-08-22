cf930d6f7964020205f1464db58879357b46da9b
karloCroDev <karlo.webdev@gmail.com>
2025-08-23 01:48:51 +0200

    Created sign up route with jwt

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -6,6 +6,7 @@ import { authRoutes } from "@/src/routes/authRoutes";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 // Routes
 app.use("/auth", authRoutes);
--- apps/backend/src/routes/authRoutes.ts
+++ apps/backend/src/routes/authRoutes.ts
@@ -1,12 +1,68 @@
 import express from "express";
 import { Router } from "express";
+import {
+  registerSchema,
+  RegisterArgs,
+  LoginArgs,
+  loginSchema,
+} from "@repo/schemas";
+import { zodErrorDetecter } from "@/src/lib/zodDetectionError";
+import bcrypt from "bcrypt";
+import { prisma } from "@/src/config/prisma";
+import jwt from "jsonwebtoken";
 
 export const authRoutes = Router();
 
-authRoutes.post("/register", (req, res) => {
-  res.send("Register route");
+authRoutes.use(express.json());
+
+authRoutes.post("/register", async (req, res) => {
+  try {
+    const data: RegisterArgs = req.body;
+
+    const validateData = registerSchema.safeParse(data);
+
+    if (!validateData.success) {
+      return res.json({
+        errors: zodErrorDetecter(validateData.error),
+      });
+    }
+
+    const hashedPassword = bcrypt.hashSync(validateData.data.password, 10);
+    const user = await prisma.user.create({
+      data: {
+        username: validateData.data.username,
+        email: validateData.data.email,
+        password: hashedPassword,
+      },
+    });
+
+    if (!user) {
+      return res.status(200).json({
+        error: "Already existing user, please login!",
+      });
+    }
+
+    const token = jwt.sign({}, process.env.JWT_SECRET as string, {
+      expiresIn: "24h",
+    });
+
+    return res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 });
 
 authRoutes.post("/login", (req, res) => {
+  try {
+    const data: LoginArgs = req.body;
+    const validateData = loginSchema.safeParse(data);
+
+    if (!validateData.success) {
+      return res.status(400).json({
+        errors: zodErrorDetecter(validateData.error),
+      });
+    }
+  } catch (error) {}
   res.send("sss");
 });
